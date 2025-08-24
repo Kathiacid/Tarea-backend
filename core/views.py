36a9cd2de869907e1d495452999885c931b52d9f
kat <kathiacid98@gmail.com>
2025-08-24 12:16:19 -0400

    uwu

--- core/views.py
+++ core/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 def home(request):
     return render(request,"core/home.html")
 
-def gallery(request):
-    return render(request,"core/gallery.html")
+def about(request):
+    return render (request,"core/about.html")
+
+def contact(request):
+    return render (request,"core/contact.html")
+
+def shop(request):
+    return render (request,"core/shop.html")
--- web1/urls.py
+++ web1/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from core import views 
 
 urlpatterns = [
-    path('',views.home,name="home"),
-    path('gallery/',views.gallery,name="gallery"),
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
+    path('shop/', views.shop, name="shop"),
     path('admin/', admin.site.urls),
 ]
